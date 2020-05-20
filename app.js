8597aa52fc27f1a6e725d30afe0f23f9f2a943fc
HoopDead <polec0@wp.pl>
2020-05-20 19:15:20 +0200

    Documatation

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+/*
+Game objects,
+functions:
+    turn {
+        param-in: event - (event is this case is just a click event used on box on website)
+        param-out: -
+        desc: Function is checking, if game is not end yet and if the box is not filled - there's a easy way to do that - first it check if event target have
+        value in innerHTML, and then it check if actualTurn is less than 0 - (if player win, turn is -2, if there's a tie, turn is -1).
+    },
+    checkWinner {
+        param-in: -
+        param-out: -
+        desc: Function is checking if there is a winner in this turn.
+    },
+    reload {
+        param-in: -
+        param-out: -
+        desc: Reloads a location
+    }
+
+*/
+
 const game = {
     player1: "far fa-circle",
     player2: "fas fa-times",
@@ -14,7 +36,7 @@ const game = {
     },
     checkWinner: function() {
         let [p1, p2] = [game.player1_moves.map(Number), game.player2_moves.map(Number)];
-        if(game.win_combinations.map(arr => arr.every(val => p1.includes(val))).some(x => x == true)) {
+        if(game.win_combinations.map(arr => arr.every(val => p1.includes(val))).some(x => x == true))  { //Check, if there's a win combination in player 1 moves, first it return a array of booleans, and then it checks if there's any true bool in array
             document.getElementById("header").innerHTML = "Player 1 wins!";
             game.actualTurn = -2;
             let winCombination = game.win_combinations.map(arr => arr.every(val => p1.includes(val))).indexOf(true);
@@ -24,7 +46,7 @@ const game = {
             document.getElementById("header").innerHTML = "Player 2 wins!";
             game.actualTurn = -2;
             console.log(p2);
-            let winCombination = game.win_combinations.ap(arr => arr.every(val => p2.includes(val))).indexOf(true);
+            let winCombination = game.win_combinations.ap(arr => arr.every(val => p2.includes(val))).indexOf(true); //Check, if there's a win combination in player 2 moves, first it return a array of booleans, and then it checks if there's any true bool in array
             let markTheWinner = this.win_combinations[winCombination];
             markTheWinner.map(numOfRow => document.getElementById(`${numOfRow}`).style.backgroundColor = "#126078");
         }
